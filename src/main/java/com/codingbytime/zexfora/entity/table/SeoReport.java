f57d5430ed2e1fd80cbb3221e8891b8d6623108e
varolemre <evarol15@posta.pau.edu.tr>
2022-08-01 20:49:51 +0300

    entities

--- src/main/java/com/codingbytime/zexfora/entity/table/SeoReport.java
+++ src/main/java/com/codingbytime/zexfora/entity/table/SeoReport.java
@@ -1,5 +1,34 @@
 package com.codingbytime.zexfora.entity.table;
 
+import javax.persistence.Entity;
 
-public class SeoReport {
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class SeoReport extends BaseObject {
+
+    private boolean h1tag;
+    private boolean h2tag;
+    private boolean mediumCss;
+    private boolean mediumJs;
+    private boolean metaDescription;
+    private String title;
+    private boolean crawlability;
+    private boolean localLanguage;
+    private boolean urlSchema;
+    private boolean charset;
+    private boolean doctype;
+    private boolean favicon;
+    private boolean robotstxt;
+    private Integer externalLinks;
+    private boolean mobileOptimization;
+    private boolean isSsl;
+    private boolean gzip;
+    private boolean blackList;
+    private int indexCount;
+    private boolean siteMap;
+    private boolean imageAlt;
 }
